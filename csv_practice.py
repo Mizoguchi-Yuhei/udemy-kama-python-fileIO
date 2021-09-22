40647c3dc588ae93d0d16a17e59fa7869e2ab65e
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-09-22 23:40:56 +0900

    9-106

---
+++ csv_practice.py
@@ -0,0 +1,28 @@
+import csv
+
+# csv.reader(f)
+# with open("example.csv") as f:
+#     reader = csv.reader(f)
+#     for line in reader:
+#         # print(line)
+#         print(line[1])
+
+# csv.DictReadr(f)
+# with open("example.csv") as f:
+#     reader = csv.DictReader(f)
+#     for line in reader:
+#         print(line['Country'])
+
+# csv.writer(f)
+# with open("sample.csv", mode='w') as f:
+#     writer = csv.writer(f)
+#     writer.writerow({'value1', 'value2'})
+#     writer.writerow({'value3', 'value4'})
+
+
+# csv.DictWriter(f)
+with open("sample.csv", mode='w') as f:
+    writer = csv.DictWriter(f, ['col1', 'col2'])
+    # writer.writeheader()
+    writer.writerow({'col1': 'value1', 'col2': 'value2'})
+    writer.writerow({'col1': 'value3', 'col2': 'value4'})
